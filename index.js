5816e90440e4c8920d18a12dbe0f3171aaa7d72a
ximbong91023 <ximbong91023@users.noreply.github.com>
2018-05-23 23:04:26 +0300

    Some animations and display information testing.

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   let index = 0;
-
+  const imgWidth = 500;
   const images = Array.from(document.querySelectorAll(".left .image img"));
 
   images.forEach(function(element, i) {
-    console.log(element)
-    element.style.left = `${ 350 * i}px`;
+    element.style.left = `${ -imgWidth* i}px`;
+    if (i !== 0) element.style.opacity = 0;
   })
-  images[0].style.visibility = 'hidden';
 
   document.querySelector("section").addEventListener("click", function(event) {
-    if (event.target.classList.contains('fa-arrow-left')) {
-      index += 1;
+    if (event.target.classList.contains('fa-arrow-right')) {
+      index++;
+      setTimeout(function() {
+        images[index - 1].style.opacity = 0;
+      }, 400);
     }
-    if (event.target.classList.contains('fa-arrow-right') && index > 0) {
-      index -= 1;
+    if (event.target.classList.contains('fa-arrow-left') && index > 0) {
+      index--;
+      setTimeout(function() {
+        images[index + 1].style.opacity = 0;
+      }, 400);
     }
-    document.querySelector(".left .image").style.transform = `translateX(${ - 350 * index}px)`;
+    images[index].style.opacity = 1;
+    document.querySelector(".left .image").style.transform = `translateX(${imgWidth* index}px)`;
+    setTimeout(function() {
+      document.querySelector(".right").innerHTML =
+        `
+      <div class="name">Name: <span></span> </div>
+      <div class="title">Position: <span></span></div>
+      <div class="nationality">Nationality: <span></span></div>
+      <div class="skills">
+        <div>Skills</div>
+        <div class="skill"><span></span></div>
+        <div class="skill"><span></span></div>
+        <div class="skill"><span></span></div>
+        <div class="skill"> <span></span></div>
+      </div>
+      <div class="story">Story: <span></span></div>
+      <div class="vision">Long term vision: <span></span></div>
+      <div class="motivation">What motivates me: <span></span> </div>
+      <div class="quote">Favorite quote: <span> </span></div>
+      <div class="date">Join date: <span></span> </div>
+      `;
+      document.querySelector(".right").style.opacity = 1;
+    }, 400);
+    document.querySelector(".right").style.opacity = 0.1;
 
   });
 
+
 })
-//transform:translateX(-350px)
